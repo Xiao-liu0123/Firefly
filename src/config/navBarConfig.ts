33c9608d7067b0d27e6cdfc6752d0b06ee2b1443
Monitor <127217146+Xiao-liu0123@users.noreply.github.com>
2026-09-20 14:47:34 +0800

    Update navBarConfig.ts

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -105,21 +105,9 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 		children: [
 			{
 				name: "GitHub",
-				url: "https://github.com/CuteLeaf/Firefly",
+				url: "https://github.com/Xiao-liu0123",
 				external: true,
 				icon: "fa7-brands:github",
-			},
-			{
-				name: "Gitee",
-				url: "https://gitee.com/CuteLeaf/Firefly",
-				external: true,
-				icon: "fa7-brands:gitee",
-			},
-			{
-				name: "Firefly文档",
-				url: "https://docs-firefly.cuteleaf.cn",
-				external: true,
-				icon: "material-symbols:docs",
 			},
 		],
 	});
